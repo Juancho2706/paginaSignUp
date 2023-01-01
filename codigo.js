a68ebcba219f8f4ec300f24702485a4b663ed75d
Juan Villegas <juanmvr2706@gmail.com>
2023-01-01 18:36:15 -0300

    Base de la pagina Sign up

---
+++ codigo.js
@@ -0,0 +1,7 @@
+const $frase1 = document.querySelector('.frase1');
+
+$frase1.innerText = `This is not a real online service! You know you need something
+like this in your life to help you realize your deepest dreams.
+Sing up now to get started.
+
+You know you want to`;
