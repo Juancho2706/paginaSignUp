fcc4fb60750d14e3fc432dad2544d24029a4e600
Juan Villegas <juanmvr2706@gmail.com>
2023-01-02 17:08:37 -0300

    Version final 1.2, con posibilidad de mejoras

--- codigo.js
+++ codigo.js
@@ -6,12 +6,30 @@ Sing up <i>now</i> to get started.<br>
 You <i>know</i> you want to`;
 
 const x = document.getElementById("form1").id;
-
-
 const $dosC = document.querySelector('.dosC');
 const botoncito = document.createElement('button');
 botoncito.type = "submit";
 botoncito.setAttribute("form", x);
 botoncito.innerText = 'Create Account'
 botoncito.classList.add('botoncito');
-$dosC.append(botoncito);
+botoncito.disabled = true;
+$dosC.prepend(botoncito);
+
+const password1 = document.querySelector('#contrasena');
+const password2 = document.querySelector('#contrasenaCON');
+password1.addEventListener('blur',(e)=>{
+    soniguales();
+})
+password2.addEventListener('blur',(e)=>{
+    soniguales();
+})
+
+function soniguales(){
+    if((password1.value === password2.value) && (8 <=password1.value)){
+    document.querySelector('.botoncito').disabled = false
+    document.querySelector('.botoncito').classList.add('ready');
+}else{
+    document.querySelector('.botoncito').disabled = true
+    document.querySelector('.botoncito').classList.remove('ready');    
+}
+}
